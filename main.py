9e4efb8c00a3114d6cff3d23ddf7ca17237d220b
Santymax123 <santymax123@hotmail.com>
2021-05-25 19:28:49 +0800

    Fixed env
    Bot now goes online and says hello

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-import os
 import discord
+
+import os
+from dotenv import load_dotenv, find_dotenv
+
+client = discord.Client()
+
+
+load_dotenv(find_dotenv())
+
+@client.event
+async def on_ready():
+    print("we have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello")
+
+
+client.run(os.environ.get("TOKEN"))
